5c6bc1068a4b14afbbe99f7d7d50c0cadeb61f9c
venomireland <sean@sdunbar.me>
2018-02-15 16:11:24 +0000

    Updating to ajubatus/ajubatus.github.io@6e41732f2a2a92815656500ab20ce5b75a7c9570.

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,81 @@
+var store = [{
+        "title": "Home Network Redesign Overview",
+        "excerpt":"After finishing my Networking Technologies degree in DIT, I decided that I wanted to start a home networking revamp to prove to myself that I could and to refresh my skills. I decided that I wanted to completely redo every aspect of the network, except for my ISP connection. I...","categories": [],
+        "tags": ["Networking"],
+        "url": "https://sdunbar.me/home-network-redesign-overview/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Home Network Redesign Part 1",
+        "excerpt":"For part 1, I started with double checking the 2901 was in a working condition and wiping it of the previous owner’s information. I was shocked to find the routing information and configuration from the networking company that it used to belong to was still present on the device. After...","categories": [],
+        "tags": ["Networking"],
+        "url": "https://sdunbar.me/home-network-redesign-part-1/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Migrating From Iredmail To An Alternative Solution",
+        "excerpt":"Originally, when I registered my own domain, I decided I wanted to use it for a professional looking email to use on my CV and for applying to jobs. When I initially planned this, I had read an Ars Technica article about setting up an email server. With this info,...","categories": ["Guides"],
+        "tags": ["Personal"],
+        "url": "https://sdunbar.me/guides/migrating-from-iredmail-to-an-alternative-solution/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Migrating From Google Authenticator To Authy",
+        "excerpt":"Migrating from Google Authenticator to AuthyTwo Factor Authentication is a neccessity these days and I try to use it wherever I can, but I was concerned still over what happened if I lost my phone and thus access to my 2FA credentials. To get around this using Google Authenticator I...","categories": ["Guides"],
+        "tags": ["Personal"],
+        "url": "https://sdunbar.me/guides/Migrating-from-Google-Authenticator-to-Authy/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Mobile Applications Formal Element",
+        "excerpt":"IntroductionThe purpose of this formal element was to design an application for Android Devices that meets the following requirements: The user will use the app to take a photo of the room The user will then be able to superimpose a series of straight lines with double ended arrows onto...","categories": [],
+        "tags": [],
+        "url": "https://sdunbar.me/mobile_applications/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Final Year Project - Preliminary Report",
+        "excerpt":"IntroductionThe purpose of this Preliminary Report is to explain the objectives of the project and provide a timeline of events that need to be achieved. This will be done in the following manner: Breaking down the project into a number of tasks Describing these tasks Researching existing solutions Talking about...","categories": ["College"],
+        "tags": ["Report","Plant"],
+        "url": "https://sdunbar.me/college/final-year-project-prelim-report/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Final Year Project - Preliminary Report",
+        "excerpt":"AcknowledgementsI would like to thank Frank Duignan primarily for providing theassistance I needed with the electronics aspects of this project, mainlythe LDR usage. Without this help these areas would have been seriouslylacking. I also want to thank him for taking the time to meet with meover the project and discuss...","categories": ["College"],
+        "tags": ["Report","Plant"],
+        "url": "https://sdunbar.me/college/final-year-project/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 1",
+        "excerpt":"SummaryI’ve always wanted to grow vegetables and fruit at home, so this summer I decided to start a raised bed,where I’d be able to grow a number of plants without having to dig up the entire garden.I decided to follow the ideas of “Square Foot Gardening”. This was based around...","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week1/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 2",
+        "excerpt":"SummarySome progress this week, pumpkins have grown out more, largest buttnernut squash leaf had been damaged so was removed.Smallest butternut squash is growing a third leaf, so I guess it’s growing just fine again after transplanting.Also moved some strawberries nearby on an old bird feeder, to get more sun than...","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week2/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 3",
+        "excerpt":"SummaryMore progress this week, pumpkins have grown out even more, slugs seem to be present so I need to deal with them.Had to trim the butternut squash again as it seemed to have mildew, which is my own fault for watering improperly. Some animal had gotten into the bed one...","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week3/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 8",
+        "excerpt":"SummaryFirst update in months due to being out of the country doing training for work.The butternut squash and pumpkin have grown dramatically since, but have yet to start growing fruit. The vines from the butternut squash will need to be trained onto the trellis over the next couple of weeks....","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week8/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 11",
+        "excerpt":"SummaryFurther updates. The butternut squash has still yet to flower so I took no pictures of it, it likely won’t in time for a good harvest.The pumpkins have started to bear fruit, however since taking this picture, the largest has been damaged and removed from the plant.The pumpkin plant has...","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week11/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 13",
+        "excerpt":"SummaryMini update, there is a single pumpkin growing and it’s growing very well. I don’t think any other fruit will be able to mature in time, so I’m going to prune off any other fruit.\t\t","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week13/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 14",
+        "excerpt":"SummaryMore progress with the pumpkin, some butternut squash growth as well, but I don’t think it will grow in time.\t\t\t","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week14/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 19",
+        "excerpt":"SummaryHad to cut back the pumpkin leaves due to major infestation. I’m not sure when it will fully ripen, but I likely will cut it off next week in preparation for Halloween.Planted the onions and garlic from my revised plan! The onions are sprouting already, the garlic should take a...","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week19/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"},{
+        "title": "Square Foot Gardening, Week 21",
+        "excerpt":"SummaryFinally approaching Halloween! Today I cut the pumpkin free and set out to carve it. It was more difficult than I had anticipated, as the pumpkin was entirely solid! This is because it was underdeveloped due to poor feeding in it’s early stages.Thankfully, it was still carvable after carving away...","categories": ["Personal"],
+        "tags": ["Project","Plant","Gardening"],
+        "url": "https://sdunbar.me/personal/sfg-week21/",
+        "teaser":"https://sdunbar.me/assets/images/teaser.jpg"}]
